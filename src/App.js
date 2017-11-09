dccdf6dad5649dbde7e5245ab645c29d5d9a7ced
marcosjrs <marcos.rivera@tecnocom.es>
2017-11-09 08:47:45 +0100

    Componente Pedidos. Representación de los pedidos que se va realizando.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react'
 import tiendaController from './tiendaController';
 import Platos from './Platos';
+import Pedidos from './Pedidos';
 import './App.css';
 
 class App extends Component {
+
   render() {    
     return (
      <div className="container">
@@ -15,7 +17,10 @@ class App extends Component {
           <a href="#" className="btn btn-primary">Platos</a>
           <a href="#" className="btn btn-primary">Pedidos</a>
         </div>
+        <div className="contenedor-componentes-principales">
         <Platos />
+        <Pedidos />
+        </div>
      </div>
     );
   }
---
+++ src/Pedidos.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import tiendaController from './tiendaController';
+import { observer } from 'mobx-react';
+
+class Pedidos extends Component{
+    render(){
+        var elPedidos = [];
+        tiendaController.platos.forEach(function(element,index) {
+            if(element.cantidad > 0){
+                elPedidos.push(
+                    <div key={index} className="list-group-item">
+                        <div className="panel-body">
+                            <h4 className="TitlePlato">{element.nombre}</h4>
+                            <div className="APrecioCantidad">
+                                <span>{element.cantidad}</span>
+                                <span className="PrecioPlato">Precio: {element.cantidad * element.precio}</span>                        
+                            </div>
+                        </div>
+                    </div>
+                );
+            }
+        });
+        return(
+            <div className="container col-xs-6">
+            <div>
+                <div className="panel panel-primary">
+                    <div className="list-group Pedidos-Menu">
+                        {elPedidos}
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default observer(Pedidos);
